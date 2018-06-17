c4bd2d4ecde9d1d92a07798ac3fb5fac9f9dd786
Andre Richardson <andrerichards@Andres-MacBook-Pro.local>
2018-06-17 07:12:16 -0400

    add enzyme/first tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default () => {
-    return <div>App</div>
+    return <div>Comment Box</div>
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,3 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import {shallow} from 'enzyme'
 import App from '../App'
+import CommentBox from '../CommentBox'
+import CommentList from '../CommentList'
+
+
+it('Shows a comment box', () => {
+    const wrapped = shallow(<App />)
+    expect(wrapped.find(CommentBox).length).toEqual(1)
+})
+
+it('Shows a comment list', () => {
+    const wrapped = shallow(<App />)
+    expect(wrapped.find(CommentList).length).toEqual(1)
+})
